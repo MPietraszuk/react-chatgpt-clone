bb4777ff0a4d32e582238d8e2bb079a39cfa533a
mpietraszuk <GitHub@troz.me>
2023-06-20 10:54:13 -0700

    Removed files added styles server.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Yoo, Whuzz Upp?</li>
+          <li>Not much around here</li>
+          <li>hey wait, Happy Fathers Day!!!!!</li>
+        </ul>
+        <nav className="footer">
+            <p className="copyright">
+                <a href="https://www.markpietraszuk.com/" target="_blank" rel="noreferrer">www.MarkPietraszuk.com</a><br />
+                &copy; 2023
+                <script>
+                    new Date().getFullYear() > 2019 &&
+                        document.write('-' + new Date().getFullYear());
+                </script>
+                &nbsp;Peace, Love & Happiness<br />
+                Made with <i className="icon ion-heart color"></i> & 
+                &nbsp;<i className="icon ion-coffee color-coffee"></i> in Laguna Niguel, CA US
+            </p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>TrozGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            chat GPT Mar 14 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
