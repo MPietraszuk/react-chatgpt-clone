59c42ecaa9ec98952eeece28a046478e5192fdc7
mpietraszuk <GitHub@troz.me>
2023-06-21 19:03:11 -0700

    Added API KEY

--- server.js
+++ server.js
@@ -5,4 +5,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const API_KEY = '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';
+
+app.post('/completions', async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "How are you?"}],
+      max_tokens: 100,
+    })
+  }
+  try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+      const data = await response.json();
+      res.send(data);
+  } catch (error) {
+    console.error('>>> error ' + error);
+  }
+});
+
 app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 const App = () => {
+  const [ message, setMessage ] = useState(null)
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "Yoo, Whuzzz Up?"
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      console.log(">>> data " + data);
+    } catch(error) {
+      console.error(">>>> error " + error);
+    }
+  }
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -30,7 +51,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
             <input />
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
             chat GPT Mar 14 Version. Free Research Preview.
